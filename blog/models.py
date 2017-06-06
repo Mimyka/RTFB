0510c95177ab5d7313f2d626abac1f7aff0c0465
Mimyka <leprunenec.jeremy@gmail.com>
2017-06-06 12:17:31 +0200

    Send data to template and add groups

--- blog/models.py
+++ blog/models.py
@@ -2,22 +2,35 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to="upl")
+class DescribeModel(models.Model):
+    name = models.CharField(max_length=100)
     headline = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Post(DescribeModel):
+    Author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True) #TODO: remove null=true and add a default author id:0 gh:1
+    picture = models.ImageField(upload_to="upl")
     analytic = models.TextField(blank=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    resume = models.TextField()
+class Author(DescribeModel):
 
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    Post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.Post
