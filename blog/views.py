a65b075ab504314e3a443cd216a0aa299a4bc768
REMIDG <degrauw.remi@outlook.com>
2017-06-07 23:05:31 +0200

    Start page 'Author' and some HTML/CSS

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Analytic
+from .models import Post, Analytic, Author
 from django.db.models import Q
 
 # Create your views here.
@@ -28,3 +28,6 @@ def details(request, id):
     Q(Post__id__exact=id)
     ),
     })
+
+def author(request, id):
+    return render(request, 'blog/author.html')
