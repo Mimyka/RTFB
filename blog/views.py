2a416761ac006a88cdcf32d251dbb9eb8b18c60e
REMIDG <degrauw.remi@outlook.com>
2017-06-07 05:08:26 +0200

    Finish HTML 'index' && Start HTML 'List'

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import Post
 # Create your views here.
 
 def list(request):
-    return render(request, 'blog/list.html', {"posts": Post.objects.all()})
+    return render(request, 'blog/list.html', {"posts": Post.objects.all(), 'recent': Post.objects.order_by('-id')[:3]})
 
 def details(request, id):
     return render(request, 'blog/details.html', {"post": get_object_or_404(Post,pk=id)})
